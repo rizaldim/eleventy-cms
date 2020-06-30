2cef403f6629425a39cf50758e36975d035164a5
Rizaldi Martaputra <rizaldi.m@gmail.com>
2020-06-30 22:26:16 +0700

    Initial commit

---
+++ src/main/java/com/rizaldi/eleventycms/EleventyCmsApplication.java
@@ -0,0 +1,13 @@
+package com.rizaldi.eleventycms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleventyCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleventyCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizaldi/eleventycms/EleventyCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizaldi.eleventycms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleventyCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
